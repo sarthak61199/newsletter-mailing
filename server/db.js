2def2199654356893e661e1571ed68f4490c8a03
Sarthak Agarwal <sarthakroxxx99@gmail.com>
2023-01-28 00:14:30 +0530

    put credentials in env file

--- server/db.js
+++ server/db.js
@@ -1,8 +1,10 @@
 import mysql from "mysql2";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 export const db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  database: "email_listing",
-  password: "password",
+  database: process.env.db,
+  password: process.env.dbPass,
 });
